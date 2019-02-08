760f0d65ab34a23d5ef7bb023ed249eb41604345
Eugen Druzin <edruzin@gmail.com>
2019-02-08 10:12:08 +0100

    Use vault encoding for signed transactions

--- src/test/kotlin/com/ampnet/integration/tests/util/BlockchainUtil.kt
+++ src/test/kotlin/com/ampnet/integration/tests/util/BlockchainUtil.kt
@@ -1,9 +1,13 @@
 package com.ampnet.integration.tests.util
 
 import com.ampnet.integration.tests.backend.TransactionData
+import io.github.novacrypto.base58.Base58
 import org.web3j.crypto.Credentials
 import org.web3j.crypto.RawTransaction
 import org.web3j.crypto.TransactionEncoder
+import org.web3j.rlp.RlpEncoder
+import org.web3j.rlp.RlpList
+import org.web3j.rlp.RlpString
 import org.web3j.utils.Numeric
 
 object BlockchainUtil {
@@ -23,11 +27,25 @@ object BlockchainUtil {
                 transaction.to,
                 transaction.data
         )
-        return sign(rawTransaction, credentials)
+        val signedTransaction = sign(rawTransaction, credentials)
+        return convertToVaultEncoding(signedTransaction)
     }
 
     private fun sign(rawTx: RawTransaction, credentials: Credentials): String {
         val signedTx = TransactionEncoder.signMessage(rawTx, credentials)
         return Numeric.toHexString(signedTx)
+    }
+
+    private fun convertToVaultEncoding(signedRawTx: String): String {
+        val version = RlpString.create("1")
+        val type = RlpString.create("1")
+        val protocol = RlpString.create("eth")
+        val payload = RlpList(
+                RlpString.create(signedRawTx),
+                RlpString.create("account-identifier")
+        )
+        val rlpEncodedTx = RlpEncoder.encode(RlpList(version, type, protocol, payload))
+        val fakeChecksum = byteArrayOf(0, 0, 0, 0)
+        return Base58.base58Encode(rlpEncodedTx + fakeChecksum)
     }
 }
