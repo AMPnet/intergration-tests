3d33a72cc324446031056a8fd1652e39f7468f27
Eugen Druzin <edruzin@gmail.com>
2019-03-18 16:48:36 +0100

    Change db init script and use integration-profile for backend

--- src/test/resources/db/init_db.sql
+++ src/test/resources/db/init_db.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS crowdfunding;
 CREATE DATABASE crowdfunding ENCODING 'UTF-8';
-
-DROP USER IF EXISTS crowdfunding;
 CREATE USER crowdfunding WITH PASSWORD 'password';
 
-DROP DATABASE IF EXISTS blockchain;
 CREATE DATABASE blockchain ENCODING 'UTF-8';
-
-DROP USER IF EXISTS blockchain;
 CREATE USER blockchain WITH PASSWORD 'password';
+
+CREATE USER test WITH PASSWORD 'password';
+ALTER ROLE test CREATEROLE;
+CREATE ROLE cf_role;
+GRANT cf_role TO crowdfunding;
+GRANT cf_role TO blockchain;
