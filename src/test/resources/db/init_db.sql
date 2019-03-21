2e6e978674e2c44051d385313f6de712f6f02908
Eugen Druzin <edruzin@gmail.com>
2019-03-21 17:48:30 +0100

    Remove test user

--- src/test/resources/db/init_db.sql
+++ src/test/resources/db/init_db.sql
@@ -4,8 +4,6 @@ CREATE USER crowdfunding WITH PASSWORD 'password';
 CREATE DATABASE blockchain ENCODING 'UTF-8';
 CREATE USER blockchain WITH PASSWORD 'password';
 
-CREATE USER test WITH PASSWORD 'password';
-ALTER ROLE test CREATEROLE;
 CREATE ROLE cf_role;
 GRANT cf_role TO crowdfunding;
 GRANT cf_role TO blockchain;
